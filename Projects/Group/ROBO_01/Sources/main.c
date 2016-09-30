6b594ede5ed5e59a5345aaee586039cfd68c656d
Raphael <raphael.andonie@bluewin.ch>
2016-09-30 16:37:14 +0200

    - Add Robo files

--- Projects/Group/ROBO_01/Sources/main.c
+++ Projects/Group/ROBO_01/Sources/main.c
@@ -30,6 +30,10 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "LEDPin1.h"
+#include "BitIoLdd1.h"
+#include "LEDPin2.h"
+#include "BitIoLdd2.h"
 #include "WAIT1.h"
 #include "CS1.h"
 #include "HF1.h"
@@ -43,7 +47,7 @@
 #include "IO_Map.h"
 /* User includes (#include below this line is not maintained by Processor Expert) */
 #include "Application.h"
-
+#include "LED.h"
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
@@ -56,6 +60,8 @@ int main(void)
 
   /* Write your code here */
   /* For example: for(;;) { } */
+  LED1_On();
+  WAIT1_Waitms(100);
   APP_Start();
 #if 0
   Bit1_ClrVal();
